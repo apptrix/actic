5d1e69ddaa95761034e916bb333fc6954a891ae2
Apptrix <info@apptrix.net>
2010-08-17 08:23:04 +0100

    Preparing rails engine

--- lib/generators/actic/install/install_generator.rb
+++ lib/generators/actic/install/install_generator.rb
@@ -3,7 +3,7 @@ require 'active_support/secure_random'
 module Actic
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", __FILE__)
+      source_root File.expand_path("../templates", __FILE__)
 
       desc "Creates an Actic initializer and copy locale files to your application."
       class_option :orm
@@ -13,11 +13,11 @@ module Actic
       end
 
       def copy_locale
-        copy_file "../../../config/locales/en.yml", "config/locales/actic.en.yml"
+        copy_file "../../../../../config/locales/en.yml", "config/locales/actic.en.yml"
       end
 
       def show_readme
-        readme "README" if behavior == :invoke
+        readme "README"
       end
     end
   end
---
+++ lib/generators/actic_generator.rb
@@ -0,0 +1,2 @@
+# Remove this file on next rails release
+require "generators/actic/actic/devise_generator"
